067b926874d2ebb9ee98c59e86263a9d3529d2ec
mahabeer123 <mahabeerpatnaik@gmail.com>
2024-08-27 14:23:01 +0530

    added files

---
+++ src/components/list/chatList/ChatList.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react"
+import "./chatList.css"
+
+const ChatList = () => {
+    const [addMod,setAddMode]=useState(false)
+  return (
+    <div className='chatList' >
+            <div className='search'>
+                <div className='searchBar'>
+                    <img src="./search.png" alt="" />
+                    <input type="text" placeholder="Search" />
+                </div>
+                <img src={addMod ?"./minus.png" : "./plus.png"} alt="" className="add" onClick={() => setAddMode((prev) => !prev)} />
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>Jane Doe</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>Jane Doe</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>Jane Doe</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>Jane Doe</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>Jane Doe</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+        </div>
+  )
+}
+
+export default ChatList
